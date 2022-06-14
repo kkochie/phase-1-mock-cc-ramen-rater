fb266f0230f14da3c9fa578ba423cfff21b7d351
Kristine Kochie <kristinekochie@gmail.com>
2022-06-14 11:08:55 -0400

    Add ramen images

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-// write your code here
+document.addEventListener('DOMContentLoaded', () => {
+//Get all ramen
+    fetch('http://localhost:3000/ramens')
+    .then(res => res.json())
+    .then(data => data.forEach(displayRamenImage))
+
+//Add ramen images
+    function displayRamenImage(ramens) {
+        const img = document.createElement('img')
+        img.src = ramens.image
+
+        document.querySelector('#ramen-menu').append(img)
+    } 
+
+})
+
+
