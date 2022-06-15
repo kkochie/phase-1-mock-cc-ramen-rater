56a17d217ccb60522d0bc0f358af0199b043af3c
Kristine Kochie <kristinekochie@gmail.com>
2022-06-14 22:23:09 -0400

    Second attempt - now working

--- src/index.js
+++ src/index.js
@@ -1,17 +1,72 @@
 document.addEventListener('DOMContentLoaded', () => {
-//Get all ramen
+
     fetch('http://localhost:3000/ramens')
     .then(res => res.json())
-    .then(data => data.forEach(displayRamenImage))
+    .then((ramenObj) => {
+        ramenObj.forEach( ramenObj => {
+            //Create ramen images
+            const ramenImage = document.createElement('img')
+            ramenImage.src = ramenObj.image
+            
+            //Add event listener to update main image and info
+            ramenImage.addEventListener('click', () => {
+                const imageDetail = document.querySelector('.detail-image')
+                imageDetail.src = ramenObj.image
 
-//Add ramen images
-    function displayRamenImage(ramens) {
-        const img = document.createElement('img')
-        img.src = ramens.image
+                const nameDetail = document.querySelector('.name')
+                nameDetail.textContent = ramenObj.name
 
-        document.querySelector('#ramen-menu').append(img)
-    } 
+                const restaurantDetail = document.querySelector('.restaurant')
+                restaurantDetail.textContent = ramenObj.restaurant
+
+                const rating = document.querySelector('#rating-display')
+                rating.textContent = ramenObj.rating
+
+                const comment = document.querySelector('#comment-display')
+                comment.textContent = ramenObj.comment
+
+            })
+
+            //Add image to DOM
+            document.querySelector('#ramen-menu').append(ramenImage)
+        })
+        
+    })
+
+    //Add new ramen using form
+    const form = document.querySelector('#new-ramen')
+    form.addEventListener('submit', function(e){
+        e.preventDefault()
+        const ramenMenu = document.querySelector('#ramen-menu')
+        const newRamenImage = document.createElement('img')
+        newRamenImage.src = e.target.image.value
+
+        newRamenImage.addEventListener('click', () => {
+            
+            const newRamen = {
+                "name": e.target.name.value,
+                "restaurant": e.target.restaurant.value,
+                "image": e.target.image.value,
+                "rating": e.target.rating.value,
+                "comment": e.target["new-comment"].value
+            }
+            
+            const imageDetail = document.querySelector('.detail-image')
+            imageDetail.src = newRamen.image
+
+            const nameDetail = document.querySelector('.name')
+            nameDetail.textContent = newRamen.name
+
+            const restaurantDetail = document.querySelector('.restaurant')
+            restaurantDetail.textContent = newRamen.restaurant
+
+            const rating = document.querySelector('#rating-display')
+            rating.textContent = newRamen.rating
+
+            const comment = document.querySelector('#comment-display')
+            comment.textContent = newRamen.comment
+        })
+        ramenMenu.append(newRamenImage)
+    })
 
 })
-
-
